0357c025bc43be47e7f064cf62c3919d06e5cc6a
Serhii Yevtushok <ggazila@gmail.com>
2020-02-10 09:48:04 +0200

    checkstyle

--- src/main/java/mate/academy/cinema/model/Order.java
+++ src/main/java/mate/academy/cinema/model/Order.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
