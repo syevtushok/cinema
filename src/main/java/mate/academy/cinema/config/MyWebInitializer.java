d4ddbac89855e77e3736ba837275e8ac31230802
Serhii Yevtushok <ggazila@gmail.com>
2020-02-20 16:52:57 +0200

    Configure Spring Security with in memory authentication Use BCryptPasswordEncoder for storing passwords in a secured way

--- src/main/java/mate/academy/cinema/config/MyWebInitializer.java
+++ src/main/java/mate/academy/cinema/config/MyWebInitializer.java
@@ -10,7 +10,7 @@ public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletI
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{AppConfig.class};
+        return new Class[]{AppConfig.class, SecurityConfig.class};
     }
 
     @Override
